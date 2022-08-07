2e34a9d9e4983831434d9a8eb278bec799911aa0
Jason Kobuchi <jekobuchi@gmail.com>
2022-08-06 23:07:19 -0700

    Added responsive designs

--- src/components/4. Advanced/Advanced.js
+++ src/components/4. Advanced/Advanced.js
@@ -6,20 +6,23 @@ const Advanced = ({ advancedStatsData }) => {
 
     return (
         <div className="advanced-container">
-            <div className='advanced-text'>
-                <p>Advanced Statistics</p>
-                <p>Track how your links are performing across the web with our advanced statistics dashboard.</p>
+            <div className='advanced-sub'>
+                <div className='advanced-text'>
+                    <p>Advanced Statistics</p>
+                    <p>Track how your links are performing across the web with our advanced statistics dashboard.</p>
+                </div>
+                <div className='advanced-benefits'>
+                    {advancedStatsData.map((advancedStatsData) => (
+                        <SellingPoints
+                            key={advancedStatsData.id}
+                            spImage={advancedStatsData.spImage}
+                            headerText={advancedStatsData.headerText}
+                            bodyText={advancedStatsData.bodyText}
+                        />
+                    ))}
+                </div>                
             </div>
-            <div className='advanced-benefits'>
-                {advancedStatsData.map((advancedStatsData) => (
-                    <SellingPoints
-                        key={advancedStatsData.id}
-                        spImage={advancedStatsData.spImage}
-                        headerText={advancedStatsData.headerText}
-                        bodyText={advancedStatsData.bodyText}
-                    />
-                ))}
-            </div>
+
         </div>
     );
   }
--- src/components/SellingPoints/SellingPoints.js
+++ src/components/SellingPoints/SellingPoints.js
@@ -5,10 +5,12 @@ const SellingPoints = ({ spImage, headerText, bodyText }) => {
 
 
     return (
-        <div className="selling-points-container">
-            <img src={require('../../images/'+spImage+'.svg')}></img>
-            <p>{headerText}</p>
-            <p>{bodyText}</p>
+        <div className='selling-points-container'>
+            <div className='selling-img-container'>
+                <img src={require('../../images/'+spImage+'.svg')}></img>
+            </div>
+            <p className='selling-header-text'>{headerText}</p>
+            <p className='selling-body-text'>{bodyText}</p>
         </div>
     );
   }
