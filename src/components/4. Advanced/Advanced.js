6d3439b61e8ccd8f9ddfb9e5e1879a6b6b636a8c
Jason Kobuchi <jekobuchi@gmail.com>
2022-08-08 23:04:37 -0700

    Added responsive designs

--- src/components/4. Advanced/Advanced.js
+++ src/components/4. Advanced/Advanced.js
@@ -7,6 +7,7 @@ const Advanced = ({ advancedStatsData }) => {
     return (
         <div className="advanced-container">
             <div className='advanced-sub'>
+                <hr></hr>
                 <div className='advanced-text'>
                     <p className='advanced-text-head'>Advanced Statistics</p>
                     <p className='advanced-text-subhead'>Track how your links are performing across the web with our advanced statistics dashboard.</p>
